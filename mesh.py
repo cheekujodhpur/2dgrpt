c3b5187c77ed9474282873c2894adcbe248082a1
Kumar Ayush <cheekujodhpur@gmail.com>
2017-09-18 19:52:33 +0530

    starting the mesh class

---
+++ mesh.py
@@ -0,0 +1,19 @@
+
+
+# Defining the Mesh
+class Mesh:
+    """
+    The class which defines a mesh, given the corner
+    coordinates and the metric. The triangles are denser
+    in regions where curvature is higher
+    """
+    vertices = [] # the list of vertices, each a tuple, 2-tuple
+    triangles = [] # 3-tuples of ids of vertices making a triangle
+
+    def __init__(self,corners):
+        """
+        Needs:
+        corners : list of four 2-tuples which describe the corners of the mesh
+        """
+        self.vertices = self.vertices + corners
+        self.trianges = []
