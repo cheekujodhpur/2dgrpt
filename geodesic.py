bcdf1a6d29e02fbb85164a31a8e58dead28455dd
Kumar Ayush <cheekujodhpur@gmail.com>
2017-09-26 10:05:43 +0530

    making geodesic on diff_area

---
+++ geodesic.py
@@ -0,0 +1,31 @@
+# this is the file where we draw geodescis
+
+from mesh import Mesh
+import numpy as np
+
+myMesh = Mesh([(0,0),(0,1),(1,0),(1,1)], (0,0))
+myMesh.submesh(5)
+
+
+startpoint = np.array((np.random.random(), np.random.random()))
+direction = np.array((np.random.random(), np.random.random()))
+direction = direction / np.linalg.norm(direction)
+
+#TODO: Find the direction we are in
+
+# print startpoint, direction
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+theta = np.array([vertex[0] for vertex in myMesh.vertices])*np.pi
+phi = np.array([vertex[1] for vertex in myMesh.vertices])*2*np.pi
+X = np.sin(theta)*np.cos(phi)
+Y = np.sin(theta)*np.sin(phi)
+Z = np.cos(theta)
+
+triangles = np.array([list(triangle) for triangle in myMesh.triangles])
+ax.plot_trisurf(X,Y,Z,triangles=triangles,shade=True,color="gray",linewidth=2)
+plt.show()
--- mesh.py
+++ mesh.py
@@ -96,27 +96,27 @@ class Mesh:
 
 
 
-myMesh = Mesh([(0,0),(0,1),(1,0),(1,1)], (0,0))
-myMesh.submesh(5)
-
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-# X = np.array([vertex[0] for vertex in myMesh.vertices])
-# Y = np.array([vertex[1] for vertex in myMesh.vertices])
-# Z = np.zeros(len(X))
+# myMesh = Mesh([(0,0),(0,1),(1,0),(1,1)], (0,0))
+# myMesh.submesh(5)
+# 
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# # X = np.array([vertex[0] for vertex in myMesh.vertices])
+# # Y = np.array([vertex[1] for vertex in myMesh.vertices])
+# # Z = np.zeros(len(X))
+# # triangles = np.array([list(triangle) for triangle in myMesh.triangles])
+# # ax.plot_trisurf(X,Y,Z,triangles=triangles,shade=True,color="gray",linewidth=2)
+# 
+# theta = np.array([vertex[0] for vertex in myMesh.vertices])*np.pi
+# phi = np.array([vertex[1] for vertex in myMesh.vertices])*2*np.pi
+# X = np.sin(theta)*np.cos(phi)
+# Y = np.sin(theta)*np.sin(phi)
+# Z = np.cos(theta)
+# 
 # triangles = np.array([list(triangle) for triangle in myMesh.triangles])
 # ax.plot_trisurf(X,Y,Z,triangles=triangles,shade=True,color="gray",linewidth=2)
-
-theta = np.array([vertex[0] for vertex in myMesh.vertices])*np.pi
-phi = np.array([vertex[1] for vertex in myMesh.vertices])*2*np.pi
-X = np.sin(theta)*np.cos(phi)
-Y = np.sin(theta)*np.sin(phi)
-Z = np.cos(theta)
-
-triangles = np.array([list(triangle) for triangle in myMesh.triangles])
-ax.plot_trisurf(X,Y,Z,triangles=triangles,shade=True,color="gray",linewidth=2)
-
-plt.show()
+# 
+# plt.show()
