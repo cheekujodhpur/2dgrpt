c66851369391ddd506809cab823732f9a4fc6ee6
Kumar Ayush <cheekujodhpur@gmail.com>
2017-09-26 11:45:26 +0530

    reducing linewidth, improving method of startpoint, so we know the start triangle

--- geodesic.py
+++ geodesic.py
@@ -3,17 +3,32 @@
 from mesh import Mesh
 import numpy as np
 
+# assuming p1, p2, p3 are tupes
+def random_point(p1, p2, p3):
+    p1 = np.array(p1)
+    p2 = np.array(p2)
+    p3 = np.array(p3)
+    return reduce(lambda x,y:x+y*np.random.random(),[p1, p2, p3])/3.
+
 myMesh = Mesh([(0,0),(0,1),(1,0),(1,1)], (0,0))
 myMesh.submesh(5)
+num_triangle = len(myMesh.triangles)
 
+# find start point in a random triangle
+t_id = int(np.random.random()*num_triangle) #triangle_id
 
-startpoint = np.array((np.random.random(), np.random.random()))
+startpoint = random_point(
+        myMesh.vertices[myMesh.triangles[t_id][0]],
+        myMesh.vertices[myMesh.triangles[t_id][1]],
+        myMesh.vertices[myMesh.triangles[t_id][2]]
+        )
+
 direction = np.array((np.random.random(), np.random.random()))
 direction = direction / np.linalg.norm(direction)
 
 #TODO: Find the direction we are in
 
-# print startpoint, direction
+print startpoint, direction
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -27,5 +42,6 @@ Y = np.sin(theta)*np.sin(phi)
 Z = np.cos(theta)
 
 triangles = np.array([list(triangle) for triangle in myMesh.triangles])
-ax.plot_trisurf(X,Y,Z,triangles=triangles,shade=True,color="gray",linewidth=2)
+ax.plot_trisurf(X,Y,Z,triangles=triangles,color="gray",linewidth=0.2)
+
 plt.show()
