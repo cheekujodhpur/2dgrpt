d4273f5bd1784b03530ccc9655751d433d087c40
Kumar Ayush <cheekujodhpur@gmail.com>
2017-12-03 14:08:50 +0530

    diff types of metric

--- metric.py
+++ metric.py
@@ -11,14 +11,24 @@ class Metric:
     def __init__(self, origin):
        self.origin = origin
 
-    def compute_metric(self, position):
+    def compute_metric(self, position, t="polar"):
+        """
+        Compute covariant metric at a point of a given type
+        """
         o = np.array(self.origin)
         p = np.array(position)
 
         g = np.zeros((2,2))
-        g[0,0] = 1
-        v = p-o
-        g[1,1] = (np.sin(v[0]*np.pi)**2)
-        
+
+        if t=="polar":
+            g[0,0] = 1
+            v = p-o
+            g[1,1] = (np.sin(v[0]*np.pi)**2)
+
+        elif t=="a1":
+            g[0,0] = 1
+            v = p-o
+            g[1,1] = v[0]
+
         return g
         
