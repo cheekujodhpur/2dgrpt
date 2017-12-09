f9b8333cef9284e66f14628b4be79b26c7f7af44
Kumar Ayush <cheekujodhpur@gmail.com>
2017-12-09 14:09:39 +0530

    shifting certain functions to helpers

--- geodesic.py
+++ geodesic.py
@@ -4,58 +4,9 @@ import os
 from mesh import Mesh
 import numpy as np
 from scipy.integrate import odeint
-
-# assuming p1, p2, p3 are tupes
-def random_point(p1, p2, p3):
-    p1 = np.array(p1)
-    p2 = np.array(p2)
-    p3 = np.array(p3)
-    c1 = np.random.random()
-    c2 = np.random.random()
-    c3 = np.random.random()
-    return (p1*c1 + p2*c2 + p3*c3)/(c1+c2+c3)
+from helpers import *
 
 
-def check_for_incidence(triangle, point_of_intersection):
-    a = np.array(myMesh.vertices[triangle[0]])
-    b = np.array(myMesh.vertices[triangle[1]])
-    c = np.array(myMesh.vertices[triangle[2]])
-    pos = np.array(point_of_intersection)
-    if abs(np.linalg.norm(pos-a) + np.linalg.norm(pos-b) - np.linalg.norm(a-b)) < 1e-4: 
-        return True
-    elif abs(np.linalg.norm(pos-b) + np.linalg.norm(pos-c) - np.linalg.norm(b-c)) < 1e-4: 
-        return True
-    elif abs(np.linalg.norm(pos-c) + np.linalg.norm(pos-a) - np.linalg.norm(c-a)) < 1e-4: 
-        return True
-    return False
-
-def return_incidence(triangle, point_of_intersection):
-    a = np.array(myMesh.vertices[triangle[0]])
-    b = np.array(myMesh.vertices[triangle[1]])
-    c = np.array(myMesh.vertices[triangle[2]])
-    pos = np.array(point_of_intersection)
-    one = abs(np.linalg.norm(pos-a) + np.linalg.norm(pos-b) - np.linalg.norm(a-b)) 
-    two = abs(np.linalg.norm(pos-b) + np.linalg.norm(pos-c) - np.linalg.norm(b-c)) 
-    tre = abs(np.linalg.norm(pos-c) + np.linalg.norm(pos-a) - np.linalg.norm(c-a)) 
-    return min(one, two, tre)
-
-def find_closest_edge(ovmesh, pt, vertices):
-    x = int(pt[0]/0.125)
-    y = int(pt[1]/0.125) 
-    if (x,y) not in ovmesh:
-        return -1, 0
-    distance = 10000
-    minEdge = (0,0)
-    for edge in ovmesh[(x,y)]:
-        a = np.array(vertices[edge[0]])
-        b = np.array(vertices[edge[1]])
-        dist = abs(np.linalg.norm(pt-a) + np.linalg.norm(pt-b) - np.linalg.norm(a-b)) 
-        if dist < distance:
-            distance = dist
-            minEdge = edge
-
-    return minEdge, distance
-        
 
 # Functions for integration
 def f(t, y):
@@ -304,14 +255,14 @@ def do_iteration(rate=1e-4,pcol="black"):
         _phi = np.concatenate((_phi, np.linspace(startpoint[1], point_of_intersection[1], 10)))
 
         pos = point_of_intersection
-        t_id = filter(lambda x:check_for_incidence(myMesh.triangles[x], point_of_intersection),
+        t_id = filter(lambda x:check_for_incidence(myMesh.vertices, myMesh.triangles[x], point_of_intersection, 1e-4),
                 neighbours_dict[t_id])
 
         if len(t_id)==1:
             t_id = t_id[0]
         elif len(t_id)>1:
             print "Error. Too many neighbours"
-            t_id = sorted(t_id, key=lambda x:return_incidence(myMesh.triangles[x], point_of_intersection))[0]
+            t_id = sorted(t_id, key=lambda x:return_incidence(myMesh.vertices, myMesh.triangles[x], point_of_intersection))[0]
             # break
         else:
             if not flipped:
